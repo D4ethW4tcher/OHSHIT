9b8146cc9047934d938cc19eb65307a0ca466c45
colten leany <coltenleany@gmail.com>
2026-04-28 10:37:59 -0600

    simple sceneManagement

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class SceneManager : MonoBehaviour
 {
+    private int CurrentLevel = 0;
+    public Button myButton;
+    public PowerUps FilePower;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        myButton.onClick.AddListener(OnButtonClick);
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if (CurrentLevel == 0 && FilePower.WinState == "Win"){
+            SceneManager.LoadScene("SampleScene")
+            CurrentLevel += 1;
+        }
+        if (CurrentLevel == 1 && FilePower.WinState == "Win"){
+            SceneManager.LoadScene("Secound level");
+            CurrentLevel += 1;
+        }
+    }
+    void OnButtonClick(){
+        FilePower.WinState = "Win";
     }
 }
