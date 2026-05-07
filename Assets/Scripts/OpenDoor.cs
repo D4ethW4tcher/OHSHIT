5016543324665f0a1ef636a60c5035526b3aae43
D4ethW4tcher <khoenzesiger06@gmail.com>
2026-05-07 10:26:38 -0600

    door

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 public class OpenDoor : MonoBehaviour
 {
     public bool door = false;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject[] = Apples[];
 
 
     // Update is called once per frame
     void Update()
     {
-        if (door == true)
+        if (Apples[] == null)
         {
-            gameObject.SetActive(false);
+            Destroy(gameObject);
         }
     }
 }
